12e6f7e41be9cc3dfe52e53b8be28174bff8a32c
Shalur1 <100019502+Shalur1@users.noreply.github.com>
2023-11-11 17:36:56 +0300

    Add Redux and ProductsInfoSlice

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ApolloClient, InMemoryCache, ApolloProvider, gql} from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/',
+    cache: new InMemoryCache(),
+});
+
+client.query({
+    query: gql`
+      query GetCategories {
+        categories{
+            name
+         }
+      }
+    `,
+    })
+    .then((result) => console.log(result));
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
